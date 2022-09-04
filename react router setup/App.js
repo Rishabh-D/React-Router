e57256ce1e84273b71ce34ad0a012ea2ae3513b7
Rishabh-D <39628086+Rishabh-D@users.noreply.github.com>
2022-09-04 14:17:02 +0530

    Add files via upload

---
+++ react router setup/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Home from "./Home"
+import About from "./About"
+
+function App() {    
+    return (
+        <div>
+            {/* Links go here */}
+            {/* Routes go here */}
+        </div>
+    )
+}
+
+export default App
